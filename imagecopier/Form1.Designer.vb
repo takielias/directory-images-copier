a7e61a9acd492e2ad32e44167862343cd75110e8
Taki Elias <taki.elias@gmail.com>
2020-10-11 16:10:41 +0600

    Üpdated

---
+++ imagecopier/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.btnSelectFolder = New System.Windows.Forms.Button()
+        Me.imgList = New System.Windows.Forms.ListBox()
+        Me.bgw = New System.ComponentModel.BackgroundWorker()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSelectFolder
+        '
+        Me.btnSelectFolder.Location = New System.Drawing.Point(12, 12)
+        Me.btnSelectFolder.Name = "btnSelectFolder"
+        Me.btnSelectFolder.Size = New System.Drawing.Size(88, 23)
+        Me.btnSelectFolder.TabIndex = 0
+        Me.btnSelectFolder.Text = "Select Folder"
+        Me.btnSelectFolder.UseVisualStyleBackColor = True
+        '
+        'imgList
+        '
+        Me.imgList.FormattingEnabled = True
+        Me.imgList.Location = New System.Drawing.Point(12, 44)
+        Me.imgList.Name = "imgList"
+        Me.imgList.Size = New System.Drawing.Size(776, 381)
+        Me.imgList.TabIndex = 1
+        '
+        'bgw
+        '
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(187, 12)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(601, 23)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(106, 12)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "Copy"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(795, 450)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.imgList)
+        Me.Controls.Add(Me.btnSelectFolder)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Multi Image Copier For KroyBikroy.com"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents btnSelectFolder As Button
+    Friend WithEvents imgList As ListBox
+    Friend WithEvents bgw As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents btnSave As Button
+End Class
---
+++ imagecopier/Form1.vb
@@ -0,0 +1,104 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+
+Public Class Form1
+
+    Private Delegate Sub UpdateListDelegate(ByVal itemName As String)
+    Private Sub UpdateList(ByVal itemName As String)
+        If Me.InvokeRequired Then
+            Me.Invoke(New UpdateListDelegate(AddressOf UpdateList), itemName)
+        Else
+            imgList.Items.Add(itemName)
+        End If
+    End Sub
+    Private Sub btnSelectFolder_Click(sender As Object, e As EventArgs) Handles btnSelectFolder.Click
+        ' set up 
+        bgw.WorkerReportsProgress = True
+
+        bgw.WorkerSupportsCancellation = True
+
+        ProgressBar1.Value = 0
+
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            imgList.Items.Clear()
+            bgw.RunWorkerAsync(10)
+        End If
+
+        If bgw.IsBusy = False Then
+            bgw.RunWorkerAsync(10)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub bgw_DoWork(sender As Object, e As DoWorkEventArgs) Handles bgw.DoWork
+
+        Dim filesListCount As Integer = New DirectoryInfo(FolderBrowserDialog1.SelectedPath).GetFiles("*", SearchOption.AllDirectories).Count
+
+        Dim n As Integer = 1
+
+        For Each fi As FileInfo In New DirectoryInfo(FolderBrowserDialog1.SelectedPath).GetFiles("*", SearchOption.AllDirectories)
+            If Not {".jpg", ".png"}.Contains(fi.Extension) Then Continue For
+            If (fi.FullName.Contains("_original")) Then Continue For
+            If (fi.FullName.Contains("_preview")) Then Continue For
+            If (fi.FullName.Contains("_thumbnail")) Then Continue For
+
+            Call UpdateList(fi.FullName)
+            'System.Threading.Thread.Sleep(5)
+            bgw.ReportProgress((n * 100) / filesListCount)
+            n = n + 1
+        Next
+
+    End Sub
+
+    Public Sub UpdateProgress(pct As Integer)
+
+        ' ToDo: Add error checking 
+        ProgressBar1.Value = ProgressBar1.Maximum
+        ProgressBar1.Value = pct
+
+    End Sub
+
+    Private Sub bgw_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles bgw.ProgressChanged
+
+        ProgressBar1.Value = ProgressBar1.Maximum
+
+        ProgressBar1.Value = e.ProgressPercentage
+
+    End Sub
+
+    Private Sub bgw_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles bgw.RunWorkerCompleted
+        ProgressBar1.Value = 100
+        MessageBox.Show("Work Complete!")
+    End Sub
+
+    Private Sub generTeImagesIndex(ByVal FolderBrowserDialog As FolderBrowserDialog)
+        For Each fi As FileInfo In New DirectoryInfo(FolderBrowserDialog.SelectedPath).GetFiles("*", SearchOption.AllDirectories)
+            If Not {".jpg", ".png"}.Contains(fi.Extension) Then Continue For
+            imgList.Items.Add(fi.FullName)
+        Next
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+        Dim DestPath As String = Application.StartupPath & "\filtered\"
+
+        For i As Integer = 1 To imgList.Items.Count - 1
+
+            Dim dir As String = DestPath & Split(imgList.Items(i), "\")(4)
+
+            If Not Directory.Exists(dir) Then
+                Directory.CreateDirectory(dir)
+            End If
+
+            Dim file = New FileInfo(imgList.Items(i))
+
+            file.CopyTo(Path.Combine(dir, file.Name), True)
+
+        Next
+
+    End Sub
+
+End Class
